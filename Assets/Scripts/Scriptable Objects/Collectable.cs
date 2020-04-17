548a8d31b620b7f2c5b7834d0e8a8b96b43f7d99
Caleb Obeng <tootired4sleep@gmail.com>
2020-04-17 14:20:33 -0400

    assets

---
+++ Assets/Scripts/Scriptable Objects/Collectable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : ScriptableObject
+{
+    // Start is called before the first frame update
+    public string objectName;
+    public Sprite icon;
+}
