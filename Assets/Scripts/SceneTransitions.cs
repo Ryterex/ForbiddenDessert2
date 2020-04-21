4e2edd654fc759edfbbe54514a2c6be17d067bd7
Amandeep Singh <mndp9711@gmail.com>
2020-04-21 17:17:49 -0400

    started scene transition stuff

---
+++ Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+
+    public Animator transitionAnim;
+    public string sceneName;
+    public bool stairCollide = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        StartCoroutine(LoadScene());
+    //    }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "StairCollider")
+        {
+            stairCollide = true;
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(5.0f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
