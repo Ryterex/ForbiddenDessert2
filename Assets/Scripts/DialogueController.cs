a3c269c3d1896b8ed93c72fe90e7a0aacb884554
Caleb Obeng <tootired4sleep@gmail.com>
2020-04-21 21:51:57 -0400

    let the games begin

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueController : MonoBehaviour
+{
+   // Start is called before the first frame update
+    public GameObject nameText;
+    public GameObject dialogueText;
+
+    //public GameObject[] interactables;
+
+    private Queue<string> sentences;
+    private Queue<string> names;
+    private float textSpeed;
+    public Animator animator;
+
+    void Start()
+    {
+        nameText = GameObject.Find("NameText");
+        dialogueText = GameObject.Find("DialogueText");;
+        sentences = new Queue<string>();
+        names = new Queue<string>();
+        textSpeed = 0.01f;
+
+        
+            
+    }
+
+    public void StartDialogue(Dialogue dialogue){
+        animator.SetBool("IsOpen", true);
+        
+        // interactables = GameObject.FindGameObjectsWithTag("Interactable");
+        /*
+        foreach (GameObject interactable in interactables)
+        {
+            interactable.GetComponent<BoxCollider2D>().enabled = false;
+        }
+        */
+        names.Clear();
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences){
+            sentences.Enqueue(sentence);
+        }
+
+        foreach (string Name in dialogue.names)
+        {
+            names.Enqueue(Name);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence(){
+        
+        if(sentences.Count == 0){
+            EndDialogue();
+            return;
+        }
+
+        string Name = names.Dequeue();
+        nameText.GetComponent<TextMeshProUGUI>().text = Name;
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+	{
+		dialogueText.GetComponent<TextMeshProUGUI>().text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.GetComponent<TextMeshProUGUI>().text += letter;
+            yield return new WaitForSeconds(textSpeed);
+		}
+	}
+
+    void EndDialogue(){
+        animator.SetBool("IsOpen", false);
+        /*
+        foreach (GameObject interactable in interactables)
+        {
+            interactable.GetComponent<BoxCollider2D>().enabled = true;
+        }
+        */
+    }
+
+}
--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,12 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class LevelController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject[] inventory;
+    public bool flashlight;
+    public bool batteries;
+
+    public GameObject flashText;
+
+    public Animator animator;
+
+    public GameObject Map;
     void Start()
     {
+        Map = GameObject.FindGameObjectWithTag("Map");
+        flashText = GameObject.Find("FlashText");
+        flashText.SetActive(false);
+        Map.SetActive(false);
+        inventory = GameObject.FindGameObjectsWithTag("Inventory");
+
         
     }
 
@@ -17,21 +33,33 @@ public class LevelController : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Debug.DrawRay(ray.origin, ray.direction * 5, Color.yellow);
 
-        if(Input.GetMouseButtonDown(1)){
-            Debug.Log("hello");
+        if(Input.GetMouseButtonDown(0) && animator.GetBool("IsOpen")){
+            FindObjectOfType<DialogueController>().DisplayNextSentence();
         }
 
-        if(Input.GetMouseButtonDown(0)){
-            Debug.Log("Mouse being clicked");
-            /*
+        if(Input.GetKeyUp(KeyCode.M)){
+            if(!Map.activeSelf){
+                Map.SetActive(true);
+            } else{
+                Map.SetActive(false);
+            }
+        }
+
+        
+
+        //this is for when interacting with an object 
+        if(Input.GetKeyUp(KeyCode.E)){
             if(Physics.Raycast(ray,out hit,3.0f)){
                 if(hit.transform != null){
+                    Debug.Log(hit.transform.name);
+                    //for collecting an object
                     if(hit.transform.tag == "InvItem"){
-                        Debug.Log(hit.transform.name);
+                        hit.transform.GetComponent<DialogueTrigger>().TriggerDialogue();
                     }
+                    
                 }
             }
-            */
+            
         }
     }
 }
